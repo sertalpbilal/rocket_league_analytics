ce12cece1d98dfc3ca4c6c2c0b47c3d30fe250f0
Sertalp B. Cay <sertalpbilal@gmail.com>
2021-06-18 10:47:43 -0400

    Add landing page, make dashboard mobile friendly

--- static/js/index.js
+++ static/js/index.js
@@ -1,14 +1,71 @@
 
 var app = new Vue({
     el: '#app',
-    data: {},
-    computed: {},
-    methods: {}
+    data: {
+        games: [],
+        colors: ['#DE8892', 'white', '#9FDC7C'],
+        thresholds: [0, 0.5, 1]
+    },
+    computed: {
+        sorted_games() {
+            let game_copy = _.cloneDeep(this.games)
+            let col_func = d3.scaleLinear().domain(this.thresholds).range(this.colors)
+            let outcome_colors = {
+                'L': "rgba(222, 136, 146, 1)",
+                'L*': "rgba(222, 136, 146, 0.5)",
+                'W*': "rgba(159, 220, 124, 0.5)",
+                'W': "rgba(159, 220, 124, 1)"
+            }
+            game_copy.forEach((g) => {
+                g['win_color'] = col_func(g['P(Win)'] / 100)
+                g['outcome_color'] = outcome_colors[g['Outcome']]
+            })
+            return game_copy.sort((a,b) => parseInt(b['StartTime']) - parseInt(a['StartTime']))
+        }
+    },
+    methods: {
+        init_table() {
+            $("#game_list").DataTable().destroy();
+            this.$nextTick(() => {
+                $("#game_list").DataTable({
+                    "order": [],
+                    "lengthChange": true,
+                    "pageLength": 10,
+                    "searching": false,
+                    "info": true,
+                    "paging": true,
+                    "columnDefs": [],
+                    "fixedHeader": true
+                });
+            })
+        }
+    }
 })
 
-
 async function fetch_file_list() {
-    console.log("xxx")
+    return new Promise((resolve, reject) => {
+        $.ajax({
+            type: "GET",
+            url: `data/tables/scorelines.tsv`,
+            dataType: "text",
+            async: true,
+            success: function(data) {
+                tablevals = data.split('\n').map(i => i.split('\t').map(j => j.trim()));
+                keys = tablevals[0];
+                values = tablevals.slice(1);
+                let el_data = values.map(i => _.zipObject(keys, i));
+                app.games = el_data;
+                app.$nextTick(() => {
+                    app.init_table()
+                })
+            },
+            error: function(xhr, status, error) {
+                console.log(error);
+                console.error(xhr, status, error);
+                reject("No data");
+            }
+        });
+    });
 }
 
 $(document).ready(() => {
@@ -18,3 +75,5 @@ $(document).ready(() => {
             console.log('ready')
         })
 })
+
+// https://stackoverflow.com/questions/5448545/how-to-retrieve-get-parameters-from-javascript
